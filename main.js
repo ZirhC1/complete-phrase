db6f28106cc99629cdde77cc91508fa641e46d3b
ZirhC1 <145370439+ZirhC1@users.noreply.github.com>
2023-11-16 10:18:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+// complete the phrase by CW
+
+//add an event listener to the button to run the placeOrder function
+document.getElementById("btn1").addEventListener("click", aBlessingIn);
+
+//store ordercode in a function
+function aBlessingIn() {
+  alert("Disguise.");
+}
+
+// complete the phrase by CW
+
+//add an event listener to the button to run the placeOrder function
+document.getElementById("btn2").addEventListener("click", aDimeA);
+
+//store ordercode in a function
+function aDimeA() {
+  alert("Dozen.");
+}
+
+// complete the phrase by CW
+
+//add an event listener to the button to run the placeOrder function
+document.getElementById("btn3").addEventListener("click", betterLateThen);
+
+//store ordercode in a function
+function betterLateThen() {
+  alert("Sorry.");
+}
+
+// complete the phrase by CW
+
+//add an event listener to the button to run the placeOrder function
+document.getElementById("btn4").addEventListener("click", noPain);
+
+//store ordercode in a function
+function noPain() {
+  alert("No gain.");
+}
+
+// complete the phrase by CW
+
+//add an event listener to the button to run the placeOrder function
+document.getElementById("btn5").addEventListener("click", timeFlies);
+
+//store ordercode in a function
+function timeFlies() {
+  alert("When you're having fun.");
+}
